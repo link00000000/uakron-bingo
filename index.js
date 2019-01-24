aec891555d9e51c8db4887ba7fdca13d289e09de
link00000000 <crandall.logan@gmail.com>
2019-01-23 19:24:52 -0500

    Removed ribbon bar

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require('electron');
 let win;
 
 function createWindow () {
-  win = new BrowserWindow({ width: 800, height: 600, fullscreen: true});
+  win = new BrowserWindow({ width: 800, height: 600, fullscreen: true, frame: false});
   win.loadFile('index.html');
 
   win.on('closed', () => {
